e6e0958751dfe6cd052f0b1770397d4571905741
nayeem01 <nayeemabdullah00gmail.com>
2024-08-14 11:31:28 +0600

    LLM integration with websocket added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from .routers import chat, embedding
+from routers import embedding, chat
+
+load_dotenv()
 
 app = FastAPI()
 
-
-app.include_router(chat.router)
 app.include_router(embedding.router)
+app.include_router(chat.router)
--- routers/chat.py
+++ routers/chat.py
@@ -1,13 +1,84 @@
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+import os
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from typing import List
 
-router = APIRouter(
-    prefix="/chat",
-    tags=["chat"],
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from langchain_chroma import Chroma
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+from langchain_huggingface import HuggingFaceEndpoint
+
+
+router = APIRouter()
+
+template = """Use the following pieces of context to answer the question at the end. If you don't know the answer,
+just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
+{context}
+Question: {question}
+Helpful Answer:"""
+
+QA_CHAIN_PROMPT = PromptTemplate(
+    input_variables=["context", "question"],
+    template=template,
+)
+
+embeddings_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), "DB")
+embedding = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+llm = HuggingFaceEndpoint(
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    task="text-generation",
+    max_new_tokens=512,
+    do_sample=False,
+    repetition_penalty=1.03,
 )
 
 
-@router.get("/")
-async def chat():
-    print("===================")
-    return {"name": "The great Plumbus"}
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+        self.vectordb = Chroma(
+            persist_directory=embeddings_dir, embedding_function=embedding
+        )
+        self.qa_chain = RetrievalQA.from_chain_type(
+            llm,
+            retriever=self.vectordb.as_retriever(),
+            return_source_documents=True,
+            chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+        )
+
+        print("ConnectionManager initialized")
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            res = self.qa_chain.invoke(message)
+            print(res["result"])
+            await connection.send_text(res["result"])
+
+
+manager = ConnectionManager()
+
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client  says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client left the chat")
